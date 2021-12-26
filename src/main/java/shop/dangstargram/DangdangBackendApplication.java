c7b0aa131db40bd82b65b3ba9f2c0d0bc8f0437e
manijang2 <manijang2@naver.com>
2021-12-26 22:51:18 +0900

    초기 커밋

---
+++ src/main/java/shop/dangstargram/DangdangBackendApplication.java
@@ -0,0 +1,13 @@
+package shop.dangstargram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DangdangBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DangdangBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/shop/dangstargram/DangdangBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.dangstargram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DangdangBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
